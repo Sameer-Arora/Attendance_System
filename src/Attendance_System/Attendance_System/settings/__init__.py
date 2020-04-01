5953bf1adb093dd4af9447ff2339c1de2be336be
Sameer <2016csb1058@iitrpr.ac.in>
2020-04-01 19:46:36 +0530

    Added enviroment variables set in .env file in root of project and added few new packacges added the different settings for devlopment and production

---
+++ src/Attendance_System/Attendance_System/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base import *       # All Django related settings
+from .django import *       # All Django related settings
+from .third_party import *  # Celery, Django REST Framework & other 3rd parties
+from .project import *      # You custom settings
--- src/Attendance_System/manage.py
+++ src/Attendance_System/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Attendance_System.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Attendance_System.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
