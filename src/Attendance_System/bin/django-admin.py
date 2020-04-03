1f0b948211bd93d60991a7adb4ae0d07732a0639
Sameer <2016csb1058@iitrpr.ac.in>
2020-04-03 13:31:17 +0530

    Removed the binaries

--- src/Attendance_System/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/akansha/Documents/srs/Attendance_System/src/Attendance_System/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
