9e73b2c5c8353bdf54597cac6f379c262c90ad13
HarshitMalik <harshit.malik999@gmail.com>
2020-04-04 11:53:25 +0530

    mobile app ui build

---
+++ Mobile_App/haazri_app/.dart_tool/flutter_build/a19f52b1d9a2eed9796f76a554f2e78f/armeabi-v7a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /home/harshit/flutter/bin/cache/artifacts/engine/android-arm-profile/linux-x64/gen_snapshot
---
+++ Mobile_App/haazri_app/.dart_tool/flutter_build/c8e3238a9ce94ce133d65fac6356e3a0/arm64-v8a/gen_snapshot.d
@@ -0,0 +1,1 @@
+gen_snapshot.d: /home/harshit/flutter/bin/cache/artifacts/engine/android-arm64-release/linux-x64/gen_snapshot
